850e83bdaed33e1f426e453d9a73bb0d3afbd29e
Erik Bos <erik@xs4all.nl>
2021-07-14 00:08:58 +0200

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ JAVA_OPTS='-Dkarate.config.dir=./config'
 exit_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  cmd="/opt/openjdk-13/bin/java $JAVA_OPTS -jar karate.jar ${args[@]} $INPUT_OPTIONS"
+  cmd="/opt/openjdk-13/bin/java $JAVA_OPTS -jar /karate.jar ${args[@]} $INPUT_OPTIONS"
   echo " - Running: $cmd"
   printf "\n"
   eval "$cmd" || exit_code=1
