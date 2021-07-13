329328f079cb55c85e633305d62057e4586781f2
Erik Bos <erik@xs4all.nl>
2021-07-13 22:54:18 +0200

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,12 @@ echo "- Karate testing"
 IFS=';'
 mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
 
+JAVA_OPTS='-Dkarate.config.dir=./config'
+
 exit_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  cmd="/opa test ${args[@]} $INPUT_OPTIONS"
+  cmd="/opt/openjdk-13/bin/java $JAVA_OPTS -jar karate.jar ${args[@]} $INPUT_OPTIONS"
   echo " - Running: $cmd"
   printf "\n"
   eval "$cmd" || exit_code=1
